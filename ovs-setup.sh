f42ec85bb2e6328b71761675c3517f9fe4f96e06
megaratboy96 <alanchevelleboy@gmail.com>
2023-06-11 22:18:37 -0600

    Update ovs-setup.sh
    Fixed a major bug. The host PC didn't have internet access.

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -1,58 +1,40 @@
 #!/bin/bash
 
-# Prompt for the number of interfaces
-read -p "Enter the number of interfaces on this device: " num_interfaces
+# Define the primary network interface connected to the router
+primary_iface="enp1s0"
 
-# Prompt for the DHCP interface
-read -p "Enter the name of the DHCP interface: " dhcp_interface
+# Read the list of additional interfaces from the user
+echo "Enter the additional interface names (separated by spaces):"
+read -r additional_ifaces
 
-# Loop to gather interface names
-interfaces=()
-for ((i=1; i<=num_interfaces; i++))
-do
-    read -p "Enter the name of interface $i: " interface
-    interfaces+=("$interface")
+# Prompt for DHCP configuration for the primary interface
+echo "Configure $primary_iface for DHCP? (y/n)"
+read -r dhcp_primary
+
+# Configure the primary interface for DHCP if selected
+if [[ $dhcp_primary =~ ^[Yy]$ ]]; then
+    echo -e "auto $primary_iface\niface $primary_iface inet dhcp\n" >> /etc/network/interfaces
+else
+    echo -e "auto $primary_iface\niface $primary_iface inet manual\n" >> /etc/network/interfaces
+    echo "up ip link set dev \$IFACE up" >> /etc/network/interfaces
+fi
+
+# Configure additional interfaces
+for iface in $additional_ifaces; do
+    echo -e "auto $iface\niface $iface inet manual\nup ip link set dev \$IFACE up\n" >> /etc/network/interfaces
 done
 
-# Prompt for the path to the configuration file
-read -p "Enter the path to the configuration file: " config_file
-
-# Install Open vSwitch
-apt-get update
-apt-get install openvswitch-switch -y
-
-# Create the bridge
-ovs-vsctl add-br br0
-
-# Add interfaces to the bridge
-for interface in "${interfaces[@]}"
-do
-    ovs-vsctl add-port br0 "$interface"
-done
-
-# Enable spanning tree protocol (STP)
-ovs-vsctl set bridge br0 stp_enable=true
-
-# Modify /etc/network/interfaces
-echo "# The primary network interface" > /etc/network/interfaces
-echo "allow-hotplug $dhcp_interface" >> /etc/network/interfaces
-echo "iface $dhcp_interface inet dhcp" >> /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-
-for interface in "${interfaces[@]}"
-do
-    if [[ "$interface" != "$dhcp_interface" ]]; then
-        echo "# Additional network interface" >> /etc/network/interfaces
-        echo "auto $interface" >> /etc/network/interfaces
-        echo "iface $interface inet manual" >> /etc/network/interfaces
-        echo "  up ip link set dev \$IFACE up" >> /etc/network/interfaces
-        echo "" >> /etc/network/interfaces
-    fi
-done
-
-# Restart the network service
+# Restart networking service
 systemctl restart networking
 
-# Print the current Open vSwitch configuration
-echo "Open vSwitch configuration on this device:"
-ovs-vsctl show
+# Set up Open vSwitch
+ovs-vsctl add-br br0
+ovs-vsctl add-port br0 $primary_iface
+
+# Add additional interfaces to the bridge
+for iface in $additional_ifaces; do
+    ovs-vsctl add-port br0 $iface
+done
+
+# Restart networking service to apply Open vSwitch changes
+systemctl restart networking
