0ba76ecdeed6a8634e49fce3ab1028878ca374ea
megaratboy96 <alanchevelleboy@gmail.com>
2023-06-15 11:22:02 -0600

    Update ovs-setup.sh
    I changed this script to match what I typed in my Debian 12 system. I have a second PC to test this script on, I'll do it later. I noticed that it takes a long time for a computer to get an IP address, so please note this is a project and I am learning as I go. I want network switches that support VLANs and advanced features. If these scripts help someone, then that would be cool. This script is intended to be for myself.

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -1,61 +1,32 @@
 #!/bin/bash
 
-# Prompt for OVS setup
-read -p "Do you want to install and configure Open vSwitch? (y/n): " install_ovs
+# Prompt for the number of interfaces
+read -p "Enter the number of interfaces to add to the bridge: " num_interfaces
 
-if [[ $install_ovs == "y" ]]; then
-  # Prompt for Ethernet ports
-  echo -n "Enter the names of the Ethernet ports (space-separated): "
-  read -r ethernet_ports
+# Prompt for the interface names and store them in an array
+declare -a interfaces
+for ((i=1; i<=num_interfaces; i++))
+do
+    read -p "Enter the name of interface $i: " interface_name
+    interfaces+=("$interface_name")
+done
 
-  # Prompt for bridge IP address
-  echo -n "Enter the IP address for the bridge (e.g., 192.168.50.152/24): "
-  read -r bridge_ip
+# Prompt for the primary interface
+read -p "Enter the name of the primary interface: " primary_interface
 
-  # Prompt for static route
-  echo -n "Enter the network IP address and gateway for the static route (e.g., 192.168.50.0/24 192.168.50.1): "
-  read -r network_route gateway_route
+# Install OVS
+apt-get install openvswitch-switch openvswitch-common -y
 
-  # Install OVS
-  apt-get update
-  apt-get install -y openvswitch-switch
+# Setup bridge
+ovs-vsctl add-br br0
 
-  # Create bridge
-  ovs-vsctl add-br br0
+# Add physical ports to bridge
+for interface in "${interfaces[@]}"
+do
+    ovs-vsctl add-port br0 $interface
+done
 
-  # Add Ethernet ports to the bridge
-  for port in $ethernet_ports; do
-    ovs-vsctl add-port br0 "$port"
-  done
-
-  # Disable IP configuration on the Ethernet ports
-  for port in $ethernet_ports; do
-    ip addr flush "$port"
-  done
-
-  # Enable the bridge
-  ip link set br0 up
-
-  # Configure IP address for the bridge
-  ip addr add "$bridge_ip" dev br0
-
-  # Enable IP forwarding
-  sysctl net.ipv4.ip_forward=1
-
-  # Configure static routes
-  ip route add "$network_route" via "$gateway_route" dev br0
-
-  # Update network configuration file
-  echo "auto br0" >> /etc/network/interfaces
-  echo "iface br0 inet static" >> /etc/network/interfaces
-  echo "  address $(echo "$bridge_ip" | cut -d'/' -f1)" >> /etc/network/interfaces
-  echo "  netmask $(echo "$bridge_ip" | cut -d'/' -f2)" >> /etc/network/interfaces
-  echo "  gateway $gateway_route" >> /etc/network/interfaces
-
-  # Restart networking services
-  systemctl restart networking
-
-  echo "Open vSwitch configuration completed."
-else
-  echo "Skipping Open vSwitch installation and configuration."
-fi
+# Flush primary interface, enable DHCP for bridge, and set bridge state to up
+ip addr flush dev $primary_interface
+dhclient br0
+ip link set br0 up
