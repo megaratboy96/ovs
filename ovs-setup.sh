4ecb3d29bbbc2dd568a929b61aa172ba892ac7fb
megaratboy96 <alanchevelleboy@gmail.com>
2023-06-21 10:28:02 -0600

    Update ovs-setup.sh
    I added virtual interfaces, the script is getting more complex now.

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -1,32 +1,108 @@
 #!/bin/bash
 
-# Prompt for the number of interfaces
-read -p "Enter the number of interfaces to add to the bridge: " num_interfaces
+# Check if the script is run with administrative privileges
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root. Please run with sudo or as the root user."
+  exit 1
+fi
 
-# Prompt for the interface names and store them in an array
-declare -a interfaces
-for ((i=1; i<=num_interfaces; i++))
+# Set variables
+interfaces_file="/etc/network/interfaces"
+
+# Function to generate a random MAC address
+generate_mac_address() {
+  printf '%02x:%02x:%02x:%02x:%02x:%02x' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256))
+}
+
+# Prompt user for the number of virtual interfaces
+echo "How many virtual interfaces do you need?"
+read num_virtual_interfaces
+
+# Generate MAC addresses for virtual interfaces
+virtual_interface_names=()
+for ((i=0; i<num_virtual_interfaces; i++))
 do
-    read -p "Enter the name of interface $i: " interface_name
-    interfaces+=("$interface_name")
+  virtual_interface_names+=("veth$i")
 done
-
-# Prompt for the primary interface
-read -p "Enter the name of the primary interface: " primary_interface
 
 # Install OVS
-apt-get install openvswitch-switch openvswitch-common -y
+apt install openvswitch-switch openvswitch-common
 
-# Setup bridge
+# Setup bridge and virtual ports for the host system
 ovs-vsctl add-br br0
 
-# Add physical ports to bridge
-for interface in "${interfaces[@]}"
+# Add virtual ports to bridge and generate cron job lines
+cron_job_lines=""
+for interface_name in "${virtual_interface_names[@]}"
 do
-    ovs-vsctl add-port br0 $interface
+  veth_interface_name="${interface_name}-br0"
+  veth_mac_address=$(generate_mac_address)
+  
+  ip link add "$interface_name" address "$veth_mac_address" type veth peer name "$veth_interface_name"
+  ip link set dev "$interface_name" up
+  
+  ovs-vsctl add-port br0 "$veth_interface_name"
+  
+  cron_job_lines+="ip link set dev $interface_name address $veth_mac_address up && systemctl restart networking"$'\n'
 done
 
-# Flush primary interface, enable DHCP for bridge, and set bridge state to up
-ip addr flush dev $primary_interface
-dhclient br0
-ip link set br0 up
+# Backup the original interfaces file
+cp "$interfaces_file" "$interfaces_file.bak"
+
+# Generate the new interfaces file content
+new_interfaces_content=$(cat <<EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug veth0
+iface veth0 inet dhcp
+
+allow-hotplug veth0-br0
+iface veth0-br0 inet manual
+
+# Virtual Ethernet interfaces
+EOF
+)
+
+# Generate the interfaces content for each virtual interface
+for interface_name in "${virtual_interface_names[@]}"
+do
+  new_interfaces_content+="allow-hotplug $interface_name
+iface $interface_name inet manual
+\n\n"
+done
+
+# Generate the bridge content
+new_interfaces_content+="allow-ovs br0
+iface br0 inet manual
+    ovs-type OVSBridge"
+
+# Write the new interfaces content to the file
+echo "$new_interfaces_content" | tee "$interfaces_file" > /dev/null
+
+# Restart the networking service
+systemctl restart networking
+
+# Get the current cron jobs for the root user
+current_cron_jobs=$(crontab -l -u root)
+
+# Check if there are existing cron jobs
+if [[ -n "$current_cron_jobs" ]]; then
+  # Append the new cron job lines to the existing ones
+  new_cron_jobs="$current_cron_jobs"$'\n'"$cron_job_lines"
+else
+  # Set the new cron job lines as the only lines if there are no existing cron jobs
+  new_cron_jobs="$cron_job_lines"
+fi
+
+# Echo the new cron jobs and pipe it to the crontab command to update the root user's cron jobs
+echo "$new_cron_jobs" | crontab -u root -
+
+echo "New virtual interfaces and cron job entries added successfully!"
